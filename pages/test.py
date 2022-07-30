f7b1d0e93f378dddea2599ca7508a260ce67b2b0
tartufo <walt2001@icloud.com>
2022-07-30 00:15:34 -0500

    deploy

--- pages/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-penguins = pd.read_csv("./data/penguins_size.csv")
-penguins["sex"] = penguins['sex'].fillna(penguins['sex'].mode()[0])
-penguins.iloc[:, 2:6] = penguins.iloc[:, 2:6].fillna(
-    penguins.iloc[:, 2:6].mean())
-
-print()
